0c0989ddc111a5c2cb2e3b26e4af7463dc54b4b4
kirankaur3 <kirankaur3@github.com>
2022-10-07 01:17:47 -0700

    Tabbing issue fixed by Kiranjot Kaur

--- peer_printer.py
+++ peer_printer.py
@@ -53,14 +53,16 @@ def main():
     gary_kane = Person('Gary Kane', False)
     haoyang_li = Person('Haoyang Li', False)
     kiranjot_kaur = Person('Kiranjot Kaur', True)
+    mohammed_alchalabi = Person('Mohammed Al Chalabi', True)
     phuc_dinh = Person('Phuc Dinh', False)
     vincent_lam = Person('Vincent Lam', False)
     jonathan_camarena_camacho = Person('Jonathan Camarena Camacho', True)
     joshua_cupler = Person('Joshua Cupler', True)
 
+
     # List of people in our class (alphabetical order by first name)
 
-    peers = [alex_souv, anish_chouhan, brandon_kmiec, eduardo_lopez, gary_kane, haoyang_li, jonathan_camarena_camacho, joshua_cupler, kiranjot_kaur, phuc_dinh, vincent_lam]
+    peers = [alex_souv, anish_chouhan, brandon_kmiec, eduardo_lopez, gary_kane, haoyang_li, jonathan_camarena_camacho, joshua_cupler, kiranjot_kaur, mohammed_alchalabi, phuc_dinh, vincent_lam]
 
     # Print out people in our class
     print("Welcome to learning Git in %s %s!" % (COURSE[0], SEMESTER[0]))
@@ -108,9 +110,14 @@ def main():
         print("%s DOES NOT like pineapple pizza" % joshua_cupler.name)
 
     if kiranjot_kaur.likes_pineapple_pizza:
-	print("%s likes pineapple pizza" % kiranjot_kaur.name)
+        print("%s likes pineapple pizza" % kiranjot_kaur.name)
     else: 
 	print("%s DOES NOT like pineapple pizza" % kiranjot_kaur.name)
+
+    if mohammed_alchalabi.likes_pineapple_pizza:
+        print("%s likes pineapple pizza" % mohammed_alchalabi.name)
+    else:
+        print("%s DOES NOT like pineapple pizza" % mohammed_alchalabi.name)
 
     if phuc_dinh.likes_pineapple_pizza:
         print("%s likes pineapple pizza" % phuc_dinh.name)
